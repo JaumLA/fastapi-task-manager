0581e3f62985a46ab69a725633cc2ccc1822dd5a
JaumLA <joao.l.alvarez@gmail.com>
2026-09-22 14:46:02 -0300

    Adicionando rota de token

--- src/taskmanagementapi/main.py
+++ src/taskmanagementapi/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from taskmanagementapi import authentication
 
-from .routes import login
+from taskmanagementapi.routes import login
 
 app = FastAPI()
 
 app.include_router(login.router)
+app.include_router(authentication.router)
 
 @app.get("/")
 async def root():
