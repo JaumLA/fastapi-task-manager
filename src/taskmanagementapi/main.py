59619513713783ce97f1f20ebd82b4d35abec84c
JaumLA <joao.l.alvarez@gmail.com>
2026-09-23 08:22:17 -0300

    Router task created

--- src/taskmanagementapi/main.py
+++ src/taskmanagementapi/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from taskmanagementapi import authentication
 
-from taskmanagementapi.routes import login
+from taskmanagementapi.routes import login, task
 
 app = FastAPI()
 
 app.include_router(login.router)
 app.include_router(authentication.router)
+app.include_router(task.router)
 
 @app.get("/")
 async def root():
--- src/taskmanagementapi/routes/task.py
+++ src/taskmanagementapi/routes/task.py
@@ -1,22 +1,55 @@
+from datetime import time
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends
 
-from pydantic import Field
-from sqlalchemy import Engine
+from pydantic import BaseModel, EmailStr, Field
 
-from taskmanagementapi.db import get_engine, Task
+from sqlalchemy import insert
+from sqlmodel import Session, select
 
-class TaskRequest(Task):
-  task_name: str = Field()
+from taskmanagementapi.db import Task, get_engine
+from taskmanagementapi.authentication import get_current_user
+
+class TaskUser(BaseModel):
+  id: int
+  email: EmailStr
+
+class TaskResponse(BaseModel):
+  task_name: str
+  init_time: time | None = None
+  end_time: time | None = None
 
 router = APIRouter(prefix="/task")
 
+@router.post("/")
+def create_task(task: TaskResponse, current_user: Annotated[TaskUser, Depends(get_current_user)]):
+  TaskResponse.model_validate(task)
+  if not current_user:
+    return {"Log again"}
+
+  engine = get_engine()
+  with Session(engine) as session:
+    create_query = insert(Task).values(
+      task_name=task.task_name,
+      init_time=task.init_time,
+      end_time=task.end_time,
+      user_id=current_user.id
+    )
+  created_task = session.exec(create_query).last_inserted_params()
+  
+  session.commit()
+  return created_task
+
 @router.get("/list")
 def get_tasks(
-  user_id: Annotated[int, Query(gt=0)],
-  token: str,
-  engine: Annotated[Engine, Depends(get_engine)]
-  #check_authentication: Annotated[bool, check_authentication]
+  current_user: Annotated[TaskUser, Depends(get_current_user)]
 ):
-  pass
+  engine = get_engine()
+  if not current_user:
+    return {"Log again"}
+  with Session(engine) as session:
+    tasks_select = select(Task).where(Task.user_id == current_user.id)
+    lot = session.exec(tasks_select).all()
+    return lot
+  
